4c135dc7e1be14af97a4f392ed0121b8638f4ba5
thule <lunthu@gmail.com>
2019-12-23 15:55:47 +0700

    event 2

--- day28/script.js
+++ day28/script.js
@@ -14,8 +14,21 @@ cool.addEventListener('click', handleCool);
 /*-----*/
 const buybutts = document.querySelectorAll('.buybutts');
 
-function buyMe() {
+function buyMe(event) {
+  //console.log(button.textContent);
+  //console.log(parseFloat(event.target.dataset.price));
   console.log('Buy buy me');
+  console.log(event.target);
+  console.log(event);
+  console.log(event.currentTarget);
+  console.log(event.target === event.currentTarget);
+  //stop this event from bubbling up
+  event.stopPropagation();
 }
 
 buybutts.forEach(buybutt => {buybutt.addEventListener(('click'), buyMe)});
+window.addEventListener('click', function (event) {
+  event.stopPropagation();
+  console.log("window was click");
+  console.log(event.target);
+}, {capture: true})
