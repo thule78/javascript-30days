74c72f2161b0cc7faa00c0619dbb02a40167f38f
thule <lunthu@gmail.com>
2020-01-09 13:56:09 +0700

    second time

--- day32/script.js
+++ day32/script.js
@@ -1,19 +1,20 @@
 const terms = document.querySelector('.terms-and-conditions');
-const watch = document.querySelector('.watch');
 const button = document.querySelector('.accept');
 
 function obCallback(payload) {
-  console.log(payload[0].intersectionRatio);
+  console.log(payload);
+
   if (payload[0].intersectionRatio === 1) {
     button.disabled = false;
-    console.log('remove disabled');
+    //stop observing the button
     ob.unobserve(terms.lastElementChild);
   }
 }
+
 const ob = new IntersectionObserver(obCallback, {
   root: terms,
   threshold: 1,
-});//new
+});
 
 
 ob.observe(terms.lastElementChild);
